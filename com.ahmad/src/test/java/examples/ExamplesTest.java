5ebbe1059835f29621d9d22c16b8c8c56f2f154a
0601ahmad <ghafooriahmadfarhad@gmail.com>
2022-05-17 21:02:01 -0700

    dsfs

--- com.ahmad/src/test/java/examples/ExamplesTest.java
+++ com.ahmad/src/test/java/examples/ExamplesTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class ExamplesTest {
-
+//lesson to me brother 
     @Test
     void testParallel() {
         Results results = Runner.path("classpath:examples")
